2001f409141c454b4dd071f2f02803aa8cdfe2f0
RaphaelOdeareduo <odeareduor@mymacewan.ca>
2024-10-02 15:32:55 -0600

    Update styling

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 import { menuArray } from "/data.js";
 const mealsSection = document.querySelector(".app-meals")
+const ordersSection = document.querySelector(".app-orders")
 
 renderMeals()
-
 function renderMeals() {
     let mealsHTML = ""
 
@@ -19,11 +19,11 @@ function renderMeals() {
                         <p class="meal-price">&dollar;${item.price}</p>
                     </div>
                     <button class="btn__add-meal">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6 icon__add">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="#8b8b8b" class="size-6 icon__add">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>                 
                     </button>
-                </div>
+                    </div>
             </div>
         `
     })
